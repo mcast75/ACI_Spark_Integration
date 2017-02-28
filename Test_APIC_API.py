566efb1e9f80a6d4308f1fbad33cc0ae7995be2b
micastel <micastel@cisco.com>
2017-02-28 12:27:20 -0500

    Fixed Fixed Websocket and Subscriptions

--- Test_APIC_API.py
+++ Test_APIC_API.py
@@ -13,11 +13,17 @@ AUTH_TIMEOUT = 90
 auth_cookie = {}
 
 def cookie_good(time_auth, timeout):
+	#print "time now: "+str(time.time())+" | time authorized: "+str(time_auth)
+
 	#if the auth-cookie has expired we need to ask for a new one
-	if time.time() - time_auth >= timeout:return False
+	if time.time() - time_auth >= timeout:
+		print "***BAD COOKIE***"
+		return False
 	else: return True 
 
 def get_cookie(ip, user, password):
+
+	#print "******COOKIE GENERATED******"
 
 	url = "http://"+ip+"/api/mo/aaaLogin.json"
 
@@ -37,7 +43,11 @@ def get_cookie(ip, user, password):
 		data = json.loads(response.text)['imdata'][0]
 		token = str(data['aaaLogin']['attributes']['token'])
 		auth_cookie = {"APIC-cookie" : token}
-		login_time = int(str(data['aaaLogin']['attributes']['firstLoginTime']))
+		login_time = time.time()#int(str(data['aaaLogin']['attributes']['firstLoginTime']))
+		#print "\n\nAPIC TIME: "+str(login_time)
+		#print "MacBook Time: "+str(time.time())
+
+		#print json.dumps(json.loads(response.text), indent=2) 
 
 
 	else:
@@ -49,16 +59,15 @@ def get_cookie(ip, user, password):
 
 def post(ip, user, cookie, payload):
 	url = "http://"+ip+"/api/node/mo/.json"
-	print json.dumps(json.loads(payload))
 
 	try:
 		response = requests.request("POST", url, data=json.dumps(json.loads(payload)), cookies=cookie)
-		print
-		print '\nStatus Code '+ str(response.status_code)
-		print 'Success\n\n'
-		print json.dumps(json.loads(response.text), indent=2)
-		print
-		print
+		print '\tStatus Code: '+ str(response.status_code)
+		print '\tSuccess\n\n\n'
+		#print json.dumps(json.loads(response.text), indent=2)
+		f = open('Post_Output.txt','a')
+		f.write(time.ctime(time.time())+'\n'+json.dumps(json.loads(response.text), indent=2)+'\n\n')
+		f.close()
 	except:
 		response = requests.request("POST", url, data=json.dumps(json.loads(payload)), cookies=cookie)
 		print "ERROR - "+str(response.status_code)
@@ -69,9 +78,11 @@ def post(ip, user, cookie, payload):
 def get(url, user, cookie):
 	try:
 		response = requests.request("GET", url, cookies=cookie, verify=False)
-		print
-		print "GET RESPONSE:"
-		print json.dumps(json.loads(response.text), indent=2)
+		print '\tStatus Code: '+ str(response.status_code)
+		print '\tSuccess\n'
+		f = open('Get_Output.txt','a')
+		f.write(time.ctime(time.time())+'\n'+json.dumps(json.loads(response.text), indent=2)+'\n\n')
+		f.close()
 	except:
 		response = requests.request("GET", url, cookies=cookie, verify=False)
 		print "ERROR - "+str(response.status_code)
@@ -80,21 +91,21 @@ def get(url, user, cookie):
 		print
 
 def sub(url, user, cookie):
-	#try:
-	url = url+'?subscription=yes'
-	response = requests.request("GET", url, cookies=cookie, verify=False)
-	print json.dumps(json.loads(response.text), indent=2)
-	print 
-	print("\nSubscribed\n")
-	#except: 
-		#print 'prblem'
+	try:
+		url = url+'?subscription=yes'
+		response = requests.request("GET", url, cookies=cookie, verify=False)
+		#print json.dumps(json.loads(response.text), indent=2)
+		print 
+		print("\nSubscribed\n")
+	except: 
+		print 'prblem'
 
 def listen(websocket, blank_string):
 	while(1):
 		result = websocket.recv()
 		print 'hit'
 		blank_string += blank_string+time.ctime(time.time())+'\n'+json.dumps(json.loads(result), indent=2)+'\n\n'
-		f = open('Test_Output','w')
+		f = open('Sub_Output.txt','w')
 		f.write(blank_string)
 		f.close()
 
@@ -114,6 +125,10 @@ def read_file(filepath):
 		sys.exit(1)
 
 if __name__ == '__main__':
+	print '\n\n\n======================================'
+	print '|+++\t\t\t          +++|\n|+++      Spark For APIC App      +++|'
+	print '|+++\t\t\t          +++|'
+	print '======================================\n\n'
 
 	#Disable warnings???
 	requests.packages.urllib3.disable_warnings()
@@ -124,30 +139,36 @@ if __name__ == '__main__':
 	password = "ins3965!"
 
 	print "IP Address: "+ip_addr+"\nUsername: "+username+"\nPassword: "+password+"\n\n"
-
+	#print "****COOKIE 1*****"
 	time_auth, auth_cookie = get_cookie(ip_addr,username, password)	
-		
-	while(1):
-		#if the auth-cookie has expired we need to ask for a new one
-		if cookie_good(time_auth, AUTH_TIMEOUT) == False: time_auth, auth_cookie = get_cookie(ip_addr,username, password)
-		host = "http://"+ip_addr+"/socket"+auth_cookie['APIC-cookie']
-		ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
 
-		ws.connect("wss://"+ip_addr+"/socket"+auth_cookie['APIC-cookie'])
+	ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+
+	ws.connect("wss://"+ip_addr+"/socket"+auth_cookie['APIC-cookie'])
 		#print ip_addr
 		#print auth_cookie["APIC-cookie"]
 		#print "ws://"+ip_addr+"/socket"+auth_cookie['APIC-cookie']
-		thread.start_new_thread(listen, (ws, ''))
+	thread.start_new_thread(listen, (ws, ''))
+		
+	while(1):
+		#if the auth-cookie has expired we need to ask for a new one
+		#print "****COOKIE 2*****"
+
+		if cookie_good(time_auth, AUTH_TIMEOUT) == False: time_auth, auth_cookie = get_cookie(ip_addr,username, password)
+		host = "http://"+ip_addr+"/socket"+auth_cookie['APIC-cookie']
+		
 		
 		try:
 			choice = raw_input("Select 1 to perform GET request or 2 to perform POST request: ")	
 			if choice == '1':
 				url = read_file("get-url.txt")
+				#print "****COOKIE 3*****"
+
 				if cookie_good(time_auth, AUTH_TIMEOUT) == False: time_auth, auth_cookie = get_cookie(ip_addr,username, password)
 				get(url,'admin', auth_cookie)
-				sub_choice = raw_input("\nCreate Subscription for this [y/n]?")
+				sub_choice = raw_input("Create Subscription for this [y/n]? ")
 				if sub_choice=='y' or sub_choice=='Y':sub(url, username, auth_cookie)
-
+				print "\n\n"
 			
 			elif choice == '2':
 				payload = read_file("post-json.txt")
@@ -155,7 +176,11 @@ if __name__ == '__main__':
 				post(ip_addr,'admin', auth_cookie, payload)
 		
 		except KeyboardInterrupt :
-			print '\nSession Closed\n'
+			print '\n\n\n********************************'
+			print '***\t\t\t     ***\n***      Session Closed      ***'
+			print '***\t\t\t     ***'
+			print '********************************\n\n'
+
 			sys.exit(0)
 		
 
